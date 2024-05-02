b429e7d7ff5cc276272cedbd05b72b1eff013501
Owen Pickford <owen.pickford@snyk.io>
2024-05-02 14:33:11 -0700

    Remove flask base. Add back demo folder

--- demo/python-command-injection.py
+++ demo/python-command-injection.py
@@ -3,7 +3,7 @@ import os
 folder = input("Enter folder path/name: ")
 
 def getFolders():
-    folders = os.popen(f"ls {folder}").read().split('/n')
+    folders = os.popen(f"ls {folder}").read().split('\n')
     return list(filter(None,folders))
 
 print(getFolders())
